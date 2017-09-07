1fb1500c90b445d1bd60cb50fc63ed1706297aa2
Gavin <gavchan@users.noreply.github.com>
2017-09-06 23:56:37 -0500

    Refactor to include config for IPv6

--- README.md
+++ README.md
@@ -6,3 +6,30 @@ This project consists of a collection of python tools that can be used for auto-
 Utilities include:
 
 - `csint.py`: interface configuration tool
+
+### csint.py
+
+```
+usage: csint.py [-h] [-d DESC] [-4 IPV4] [-6 IPV6] [-l LLOCAL] [-m NETMASK]
+                [-s] [-x]
+                interface
+```
+
+**Generator utility for Cisco IOS interface configuration**
+
+```
+positional arguments:
+  interface             Interface
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DESC, --desc DESC  Description
+  -4 IPV4, --ipv4 IPV4  IPv4 address/CIDR
+  -6 IPV6, --ipv6 IPV6  IPv6 address/CIDR
+  -l LLOCAL, --llocal LLOCAL
+                        IPv6 link-local address
+  -m NETMASK, --netmask NETMASK
+                        Add dotted decimal IPv4 netmask
+  -s, --shutdown        Shutdown interface (default: no shutdown)
+  -x, --exit            Add exit to end of interface configuration
+```
--- csint.py
+++ csint.py
@@ -13,8 +13,7 @@ class cs_Interface:
     def __init__(self, id):
         self.ident = id
 
-    def __init__(self, id, ipv4addr):
-        self.ident = id
+    def add_ipv4(self, ipv4addr):
         if '/' in ipv4addr:
             self.ipv4, cidr = self.ip_cidr(ipv4addr)
             self.netmask = self.get_mask(cidr)
@@ -28,6 +27,10 @@ class cs_Interface:
         return addr, cidr
 
     def get_mask(self, cidr):
+        # Convert CIDR to dotted decimal netmmask
+        # Input: numeric string
+        # Output: Netmask in dotted decimal (string)
+
         try:
             num = int(cidr)
         except:
@@ -47,19 +50,37 @@ class cs_Interface:
                 for i in range(3-quo):
                     mask += ".0"
         return mask
+
     def add_mask(self, mask):
+        # Uses specified netmask for IPv4 address
         self.netmask = mask
 
     def add_desc(self, desc):
+        # Adds description to interface
         self.desc = desc
 
+    def add_ipv6(self, ipv6):
+        # Adds IPv6 address to interface
+        self.ipv6 = ipv6
+
+    def add_llocal(self, llocal):
+        # Adds IPv6 link-local address to interface
+        self.linklocal = llocal
+
     def no_shut(self, enable):
+        # Sets port status (default enable=true)
         self.no_shut = enable
 
     def output(self):
-        out_str = "interface {}\n ip address {} {}\n".format(self.ident, self.ipv4, self.netmask)
-        if self.desc != "":
-            out_str += "description {}\n".format(self.desc)
+        out_str = "interface {}\n".format(self.ident)
+        if self.desc:
+            out_str += " description {}\n".format(self.desc)
+        if self.ipv4:
+            out_str += " ip address {} {}\n".format(self.ipv4, self.netmask)
+        if self.ipv6:
+            out_str += " ipv6 address {}\n".format(self.ipv6)
+        if self.linklocal:
+            out_str += " ipv6 address {} link-local\n".format(self.linklocal)
         if self.no_shut:
             out_str += " no shutdown\n"
         else:
@@ -68,19 +89,44 @@ class cs_Interface:
 
 parser = argparse.ArgumentParser(description="Generator utility for Cisco IOS interface configuration")
 parser.add_argument("interface", help="Interface")
-parser.add_argument("ipv4_address", help="IPv4 address/CIDR")
-parser.add_argument("-m", "--netmask", help="Add dotted decimal netmask", type=str)
+parser.add_argument("-d", "--desc", help="Description")
+parser.add_argument("-4", "--ipv4", help="IPv4 address/CIDR", type=str)
+parser.add_argument("-6", "--ipv6", help="IPv6 address/CIDR", type=str)
+parser.add_argument("-l", "--llocal", help="IPv6 link-local address", type=str)
+parser.add_argument("-m", "--netmask", help="Add dotted decimal IPv4 netmask", type=str)
 parser.add_argument("-s", "--shutdown", help="Shutdown interface (default: no shutdown)", action="store_true")
-parser.add_argument("--desc", help="Description")
+parser.add_argument("-x", "--exit", help="Add exit to end of interface configuration", action="store_true")
 args = parser.parse_args()
 
-# Assign IP address to interface; converts CIDR to netmask
-cs_int = cs_Interface(args.interface, args.ipv4_address)
+cs_int = cs_Interface(args.interface)
 
-# Use given netmask if flag -m | --netmask provided
-if args.netmask:
-    cs_int.add_mask(args.netmask)
+if args.ipv4:
+    # Create instance of interface +/- IPv4 address/CIDR
+    cs_int.add_ipv4(args.ipv4)
+
+    if args.netmask:
+        # Use given netmask if specified
+        cs_int.add_mask(args.netmask)
+else:
+    cs_int = cs_Interface(args.interface)
+
+if args.desc:
+    # Add description to interface
+    cs_int.add_desc(args.desc)
+if args.ipv6:
+    # Add IPv6 address
+    cs_int.add_ipv6(args.ipv6)
+if args.llocal:
+    # Add IPv6 link-local address
+    cs_int.add_llocal(args.llocal)
 if args.shutdown:
+    # Explicitly shutdown port
     cs_int.no_shut(False)
-print(cs_int.output())
 
+# Generate output
+
+config = cs_int.output()
+if args.exit:
+    config += " exit\n"
+
+print(config)
